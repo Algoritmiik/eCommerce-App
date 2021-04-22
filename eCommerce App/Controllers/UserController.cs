fd7acee5887acd8a57365ac10cf501d46b0f541a
Mert Aslan <mertcanaslan198@gmail.com>
2021-04-23 00:10:14 +0300

    Login Functions Progresses

--- eCommerce App/Controllers/UserController.cs
+++ eCommerce App/Controllers/UserController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using eCommerce_App.Models.Database;
 
 namespace eCommerce_App.Controllers
 {
     public class UserController : Controller
     {
-        // GET: User
+
+        Context c = new Context();
+
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Users user)
+        {
+            var datas = c.Users.FirstOrDefault(x => x.email == user.email && x.passwordHash == user.passwordHash);
+            if(datas != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Invalid = "Email or password is wrong";
+                return View(user);
+            }
         }
 
         public ActionResult Registered()
