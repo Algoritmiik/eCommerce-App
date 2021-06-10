a612ae4fde76d1b464f1ee9b5257d679bc19499a
Tuna Çandır <58295104+tunacandir@users.noreply.github.com>
2021-06-10 03:01:23 +0300

    Profile is almost finished

--- eCommerce App/Controllers/ProfileController.cs
+++ eCommerce App/Controllers/ProfileController.cs
@@ -8,9 +8,15 @@ using eCommerce_App.Models.Database;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using System.Globalization;
+using System.Web.Security;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
 
 namespace eCommerce_App.Controllers
 {
+    [Authorize]
     public class ProfileController : Controller
     {
 
@@ -30,9 +36,64 @@ namespace eCommerce_App.Controllers
         }
 
         public ActionResult CreditCards()
+        {        
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreditCards(Users user,CreditCards card)
         {
-            var allCards = c.CreditCards.ToList(); // Where eklenecek 
-            return View(allCards);
+            if (c.CreditCards.Where(x => x.creditCardId == card.creditCardId).First() != null)
+            {
+                try
+                {
+                    var currentCardHolder = c.CreditCards.Where(x => x.creditCardId == card.creditCardId).First();
+                    var currentUser = c.Users.Where(x => x.userName == user.userName).First();
+
+                    currentCardHolder.cardNumber = card.cardNumber;
+                    currentCardHolder.creditCardName = card.creditCardName;
+                    currentCardHolder.CVV = card.CVV;
+                    currentCardHolder.holderName = card.holderName;
+                    currentCardHolder.expireDate = card.expireDate;
+                    currentCardHolder.userId = card.userId; // burada sıkıntı var user Id almıyor
+                    c.Entry(currentCardHolder).CurrentValues.SetValues(currentCardHolder);
+                    c.SaveChanges();
+                }
+                catch (DbEntityValidationException e)
+                {
+                    foreach (var eve in e.EntityValidationErrors)
+                    {
+                        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                        foreach (var ve in eve.ValidationErrors)
+                        {
+                            Console.WriteLine("- Property: \"{0}\", Value: \"{1}\", Error: \"{2}\"",
+                                ve.PropertyName,
+                                eve.Entry.CurrentValues.GetValue<object>(ve.PropertyName),
+                                ve.ErrorMessage);
+                        }
+                    }
+                    throw;
+                }
+               
+            }
+            else if (c.CreditCards.Where(x => x.creditCardId == card.creditCardId).First() == null)
+            {
+                var newCardHolder = c.CreditCards.Where(x => x.userId == card.userId).First();
+                newCardHolder.cardNumber = card.cardNumber;
+                newCardHolder.creditCardName = card.creditCardName;
+                newCardHolder.CVV = card.CVV;
+                newCardHolder.holderName = card.holderName;
+                newCardHolder.expireDate = card.expireDate;
+                newCardHolder.userId = card.userId;
+                c.CreditCards.Add(newCardHolder);
+                c.SaveChanges();
+            }
+            else
+            {
+                return RedirectToAction("Profile");
+            }
+            return RedirectToAction("Profile");
         }
 
         [HttpPost]
@@ -43,8 +104,7 @@ namespace eCommerce_App.Controllers
 
         public ActionResult Addresses()
         {
-            var allAddresses = c.UserAddresses.ToList();
-            return View(allAddresses);
+            return View();
         }
 
         [HttpPost]
@@ -59,10 +119,25 @@ namespace eCommerce_App.Controllers
         }
 
         [HttpPost]
+        public ActionResult UpdateInfo(Users user)
+        {
+            var currentUser = c.Users.Where(x => x.userName == user.userName).First();
+            currentUser.firstName = user.firstName;
+            currentUser.lastName = user.lastName;
+            currentUser.phoneNumber = user.phoneNumber;
+            currentUser.email = user.email;
+            c.Entry(currentUser).CurrentValues.SetValues(currentUser);
+            c.SaveChanges();
+            return RedirectToAction("Profile");
+        }
+
+        [HttpPost]
         public ActionResult UpdateAccountRedirect()
         {
+            
             return RedirectToAction("UpdateInfo");
         }
+
 
         public ActionResult ResetPassword()
         {
@@ -70,63 +145,21 @@ namespace eCommerce_App.Controllers
         }
 
         [HttpPost]
+        public ActionResult ResetPassword(Users user)
+        {
+            string password = SHA256Hashing(user.passwordHash);
+            var currentUser = c.Users.Where(x => x.userName == user.userName).First();
+            currentUser.passwordHash = password;
+            c.Entry(currentUser).CurrentValues.SetValues(currentUser);
+            c.SaveChanges();
+            return RedirectToAction("Profile");
+        }
+
+
+        [HttpPost]
         public ActionResult ResetPasswordRedirect()
         {
             return RedirectToAction("ResetPassword");
-        }
-
-        public ActionResult Orders()
-        {
-            return View();
-        }
-
-        public ActionResult CurrentOrders()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult CurrentOrdersRedirect()
-        {
-            return RedirectToAction("CurrentOrders");
-        }
-
-        public ActionResult PastOrders()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult PastOrdersRedirect()
-        {
-            return RedirectToAction("PastOrders");
-        }
-
-        public ActionResult RatingsComments()
-        {
-            return View();
-        }
-
-        public ActionResult Ratings()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult RatingsRedirect()
-        {
-            return RedirectToAction("Ratings");
-        }
-
-        public ActionResult Comments()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult CommentsRedirect()
-        {
-            return RedirectToAction("Comments");
         }
 
         public string SHA256Hashing(string password)
